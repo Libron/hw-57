31a1111f2088c18fc69e620a231325734d664493
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-11 03:43:02 +0600

    added Submit handler

--- src/components/ControlPanel/ControlPanel.js
+++ src/components/ControlPanel/ControlPanel.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ControlPanel = props => {
     return (
         <form className="Controls" onSubmit={props.onSubmit}>
-            <input type="text" className="Item-name" placeholder="Item name" />
-            <input type="text" className="Item-cost" placeholder="Cost"/>
+            <input type="text" className="Item-name" onChange={props.nameExpense} placeholder="Item name" />
+            <input type="text" className="Item-cost" onChange={props.costExpense} placeholder="Cost"/>
             <label htmlFor="Item-cost">KGS</label>
             <button type="submit" className="btn-submit">Add item</button>
         </form>
