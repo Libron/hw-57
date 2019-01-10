408021433b04a9ceb6f6158d9d55621a86d84fc5
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-11 03:43:35 +0600

    Rendering and styling contol panel and expenses list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
+import ControlPanel from "./components/ControlPanel/ControlPanel";
 
 class App extends Component {
+  state = {
+    expenses: [],
+      name: null,
+      cost: 0,
+    total: 0
+  };
+
+    addExpense = event => {
+      event.preventDefault();
+      if (this.state.name && this.state.cost) {
+          const expenses = this.state.expenses;
+          expenses.push({name: this.state.name, cost: this.state.cost});
+          this.setState(expenses);
+      }
+    };
+
+    setExpenseNameHandler = event => {
+        this.setState({name: event.target.value});
+    };
+
+    setExpenseCostHandler = event => {
+        this.setState({cost: parseInt(event.target.value)});
+    };
+
   render() {
     return (
       <div className="App">
-
+        <ControlPanel
+            onSubmit={event => this.addExpense(event)}
+            nameExpense={this.setExpenseNameHandler}
+            costExpense={this.setExpenseCostHandler}
+        />
+        <div className="Expenses">
+          <ul>
+              {this.state.expenses.map((item, index) => {
+                  return (<li key={index}>{item.name}</li>);
+              })}
+            <li>Milk</li>
+            <li>Milk</li>
+            <li>Milk</li>
+            <li>Milk</li>
+          </ul>
+        </div>
       </div>
     );
   }
