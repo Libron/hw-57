86ab2d4dda582e53e0f01b245f623bb89ecf13cb
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-11 07:01:01 +0600

    Code refactor for the ProgressBar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -11,14 +11,13 @@ const ProgressBar = props => {
         return acc;
     }, {});
 
-
     return (
         <React.Fragment>
             <h3>Progress Bar:</h3>
             <div className="ProgressBar">
                 {Object.keys(category).reduce((acc, key) => {
-                    let barWidth = category[key] * 100 / props.total;
-                    acc.push(<div key={key} className={key} style={{width: barWidth + '%'}}> {category[key] + ' KGS'}</div>);
+                    let styleWidth = category[key] * 100 / props.total;
+                    acc.push(<div key={key} className={key} style={{width: styleWidth + '%'}}> {category[key] + ' KGS'}</div>);
                     return acc;
                 }, [])}
             </div>
